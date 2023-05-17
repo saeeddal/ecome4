f0d2643bc2d7059ffb0bcac7cd3fbd0fd40374f9
saeed dalvand <40673352+saeeddal@users.noreply.github.com>
2023-05-17 20:07:03 +0330

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <h3>this is main header from layout</h3>
+      <nav class="flex align-center gap-4 p-4">
+        <NuxtLink to="/">home page </NuxtLink>
+        <NuxtLink to="/about">about </NuxtLink>
+      </nav>
+    </header>
+    <slot />
+    <footer>
+      <h3>this is main footer from layout</h3>
+    </footer>
+  </div>
+</template>
+<style slot>
+header {
+  background-color: blueviolet;
+  color: aliceblue;
+}
+footer {
+  background-color: gray;
+  color: aliceblue;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3>base component</h3>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
