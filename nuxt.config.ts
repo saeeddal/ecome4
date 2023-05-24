a373a55f5e5a19d28819a1fee226157357ff9034
saeed dalvand <40673352+saeeddal@users.noreply.github.com>
2023-05-24 22:55:59 +0330

    add fonts to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Nuxt 3 App',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=2',
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Ubuntu',
+        },
+      ],
+    },
+  },
 });
